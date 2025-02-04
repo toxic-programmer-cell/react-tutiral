71ed08d6763563ead4e17f71bed02f9dd97e633c
Rohit Kumar Mahto <rohitmahto062@gmail.com>
2025-02-04 21:42:33 +0530

    Hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  const [text, setText] = useState('');
+  const handleIncrement = () => setCounter(counter + 1);
+  const handleDecrement = () => setCounter(counter - 1);
+  const handleReset = () => setCounter(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{textAlign: 'center'}}>
+
+      <h1 style={{color:'Red'}}> # useState Hook</h1>
+      <p style={{color:'black'}}>----------------------------------------------</p>
+      {/* COUNTER */}
+
+      <h2 style={{color:'blue', textTransform: 'uppercase'}}>*Counter</h2>
+      <button class="btn btn-primary" onClick={handleReset}>Reset</button>
+      <h2>Counter: {counter}</h2>
+      <button class="btn btn-secondary me-4" onClick={handleIncrement}>Increment</button>
+      <button class="btn btn-secondary" onClick={handleDecrement}>Decrement</button>
+
+      <h2 className='mt-4' style={{color:'blue', textTransform: 'uppercase'}}>*Text</h2>
+      <input class="form-control border-primary bg-light" type="text" onChange={e => setText(e.target.value)} />
+      <h3 class="text-center fst-italic text-primary mt-4">{text}</h3>
     </div>
-  );
-}
+  )
+};
 
 export default App;
+
